987654a9620831829dd69b380be73396ea1cf3d5
tamaoki <treasure6hunter9cole@gmail.com>
2020-08-02 11:17:28 +0900

    first commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by 玉置 on 2020/08/02.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ChatApp
+//
+//  Created by 玉置 on 2020/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
